aadd7d637c701401f3a6824a30b50733e757b2f3
itiligent <94789708+itiligent@users.noreply.github.com>
2022-07-16 19:44:51 +1000

    Update create-usb-bootable-ubuntu.sh

--- create-usb-bootable-ubuntu.sh
+++ create-usb-bootable-ubuntu.sh
@@ -1,146 +1,146 @@
 #!/bin/bash
 
-# Safety check, un as sudo
+# Safety check, run as sudo
 if [ $(id -u) != 0 ]; then
-   echo "Safety check:  This scipt is meant to be an as oot o sudo.  Please un using sudo ./BootFix.sh.  Exiting..."
+   echo "Safety check:  This script is meant to be ran as root or sudo.  Please run using sudo ./BootFix.sh.  Exiting..."
    exit 1
 fi
 
-# Safety check -- check fo system-boot automount
+# Safety check -- check for system-boot automount
 if [ -d /media/*/system-boot ]; then
-   echo "Safety check:  automount detected at /media/*/system-boot.  Please unmount the automount in File Exploe o with sudo umount /media/$LOGNAME/system-boot."
+   echo "Safety check:  automount detected at /media/*/system-boot.  Please unmount the automount in File Explorer or with sudo umount /media/$LOGNAME/system-boot."
    exit 1
 fi
 
-# Safety check -- check fo witable automount
-if [ -d /media/*/witable ]; then
-   echo "Safety check:  automount detected at /media/*/witable.  Please unmount the automount in File Exploe o with sudo umount /media/$LOGNAME/system-boot."
+# Safety check -- check for writable automount
+if [ -d /media/*/writable ]; then
+   echo "Safety check:  automount detected at /media/*/writable.  Please unmount the automount in File Explorer or with sudo umount /media/$LOGNAME/system-boot."
    exit 1
 fi
 
-# Find the "witable" oot filesystem mount
-if [ -d /mnt/witable ] && [ -d /mnt/witable/us/lib/u-boot ] ; then
-    mntWitable='/mnt/witable'
+# Find the "writable" root filesystem mount
+if [ -d /mnt/writable ] && [ -d /mnt/writable/usr/lib/u-boot ] ; then
+    mntWritable='/mnt/writable'
 else
-    echo "The patition 'witable' was not found in /mnt/witable.  Make sue you have mounted you USB mass stoage device (ex: sudo mount /dev/sda2 /mnt/witable)."
+    echo "The partition 'writable' was not found in /mnt/writable.  Make sure you have mounted your USB mass storage device (ex: sudo mount /dev/sda2 /mnt/writable)."
     exit 1
 fi
-echo "Found witable patition at $mntWitable"
+echo "Found writable partition at $mntWritable"
 
 # Find the "system-boot" boot filesystem mount
 if [ -d /mnt/boot ] && [ -e /mnt/boot/vmlinuz ]; then
     mntBoot='/mnt/boot'
 else
-    echo "The 'boot' patition was not found in /mnt/boot.  Make sue you have mounted you USB mass stoage device (ex: sudo mount /dev/sda1 /mnt/boot)."
+    echo "The 'boot' partition was not found in /mnt/boot.  Make sure you have mounted your USB mass storage device (ex: sudo mount /dev/sda1 /mnt/boot)."
     exit 1
 fi
-echo "Found boot patition at $mntBoot"
+echo "Found boot partition at $mntBoot"
 
-# Decompess the kenel
-echo "Decompessing kenel fom vmlinuz to vmlinux..."
+# Decompress the kernel
+echo "Decompressing kernel from vmlinuz to vmlinux..."
 zcat -qf "$mntBoot/vmlinuz" > "$mntBoot/vmlinux"
-echo "Kenel decompessed"
+echo "Kernel decompressed"
 
-# Check if 32 bit o 64 bit and modify config.txt
-if cat "$mntBoot/config.txt" | gep -q "am_64bit=1"; then
+# Check if 32 bit or 64 bit and modify config.txt
+if cat "$mntBoot/config.txt" | grep -q "arm_64bit=1"; then
 
-# Update config.txt with coect paametes
-echo "Updating config.txt with coect paametes..."
+# Update config.txt with correct parameters
+echo "Updating config.txt with correct parameters..."
 
 cat <<EOF | sudo tee "$mntBoot/config.txt">/dev/null
 # Please DO NOT modify this file; if you need to modify the boot config, the
-# usecfg.txt file is the place to include use changes. Please efe to
-# the README file fo a desciption of the vaious configuation files on
-# the boot patition.
+# usercfg.txt file is the place to include user changes. Please refer to
+# the README file for a description of the various configuration files on
+# the boot partition.
 
-# The unusual odeing below is delibeate; olde fimwaes (in paticula the
-# vesion initially shipped with bionic) don't undestand the conditional
-# [sections] below and simply ignoe them. The Pi4 doesn't boot at all with
-# fimwaes this old so it's safe to place at the top. Of the Pi2 and Pi3, the
-# Pi3 uboot happens to wok happily on the Pi2, so it needs to go at the bottom
-# to suppot old fimwaes.
+# The unusual ordering below is deliberate; older firmwares (in particular the
+# version initially shipped with bionic) don't understand the conditional
+# [sections] below and simply ignore them. The Pi4 doesn't boot at all with
+# firmwares this old so it's safe to place at the top. Of the Pi2 and Pi3, the
+# Pi3 uboot happens to work happily on the Pi2, so it needs to go at the bottom
+# to support old firmwares.
 
 [pi4]
-max_famebuffes=2
-dtovelay=vc4-fkms-v3d
+max_framebuffers=2
+dtoverlay=vc4-fkms-v3d
 boot_delay
-kenel=vmlinux
-initamfs initd.img followkenel
+kernel=vmlinux
+initramfs initrd.img followkernel
 
 [pi2]
 boot_delay
-kenel=vmlinux
-initamfs initd.img followkenel
+kernel=vmlinux
+initramfs initrd.img followkernel
 
 [pi3]
 boot_delay
-kenel=vmlinux
-initamfs initd.img followkenel
+kernel=vmlinux
+initramfs initrd.img followkernel
 
 [all]
-am_64bit=1
-device_tee_addess=0x03000000
+arm_64bit=1
+device_tree_address=0x03000000
 
-# The following settings ae defaults expected to be oveidden by the
-# included configuation. The only eason they ae included is, again, to
-# suppot old fimwaes which don't undestand the include command.
+# The following settings are defaults expected to be overridden by the
+# included configuration. The only reason they are included is, again, to
+# support old firmwares which don't understand the include command.
 
-enable_uat=1
+enable_uart=1
 cmdline=cmdline.txt
 
 include syscfg.txt
-include usecfg.txt
+include usercfg.txt
 
 EOF
 
 # End 64 bit
 else
 
-# Update config.txt with coect paametes
-echo "Updating config.txt with coect paametes..."
+# Update config.txt with correct parameters
+echo "Updating config.txt with correct parameters..."
 
 cat <<EOF | sudo tee "$mntBoot/config.txt">/dev/null
 # Please DO NOT modify this file; if you need to modify the boot config, the
-# usecfg.txt file is the place to include use changes. Please efe to
-# the README file fo a desciption of the vaious configuation files on
-# the boot patition.
+# usercfg.txt file is the place to include user changes. Please refer to
+# the README file for a description of the various configuration files on
+# the boot partition.
 
-# The unusual odeing below is delibeate; olde fimwaes (in paticula the
-# vesion initially shipped with bionic) don't undestand the conditional
-# [sections] below and simply ignoe them. The Pi4 doesn't boot at all with
-# fimwaes this old so it's safe to place at the top. Of the Pi2 and Pi3, the
-# Pi3 uboot happens to wok happily on the Pi2, so it needs to go at the bottom
-# to suppot old fimwaes.
+# The unusual ordering below is deliberate; older firmwares (in particular the
+# version initially shipped with bionic) don't understand the conditional
+# [sections] below and simply ignore them. The Pi4 doesn't boot at all with
+# firmwares this old so it's safe to place at the top. Of the Pi2 and Pi3, the
+# Pi3 uboot happens to work happily on the Pi2, so it needs to go at the bottom
+# to support old firmwares.
 
 [pi4]
-max_famebuffes=2
-dtovelay=vc4-fkms-v3d
+max_framebuffers=2
+dtoverlay=vc4-fkms-v3d
 boot_delay
-kenel=vmlinux
-initamfs initd.img followkenel
+kernel=vmlinux
+initramfs initrd.img followkernel
 
 [pi2]
 boot_delay
-kenel=vmlinux
-initamfs initd.img followkenel
+kernel=vmlinux
+initramfs initrd.img followkernel
 
 [pi3]
 boot_delay
-kenel=vmlinux
-initamfs initd.img followkenel
+kernel=vmlinux
+initramfs initrd.img followkernel
 
 [all]
-device_tee_addess=0x03000000
+device_tree_address=0x03000000
 
-# The following settings ae defaults expected to be oveidden by the
-# included configuation. The only eason they ae included is, again, to
-# suppot old fimwaes which don't undestand the include command.
+# The following settings are defaults expected to be overridden by the
+# included configuration. The only reason they are included is, again, to
+# support old firmwares which don't understand the include command.
 
-enable_uat=1
+enable_uart=1
 cmdline=cmdline.txt
 
 include syscfg.txt
-include usecfg.txt
+include usercfg.txt
 
 EOF
 
@@ -148,61 +148,55 @@ EOF
 fi
 
 
-# Ceate scipt to automatically decompess kenel (souce: https://www.aspbeypi.og/foums/viewtopic.php?t=278791)
-echo "Ceating scipt to automatically decompess kenel..."
-cat << \EOF | sudo tee "$mntBoot/auto_decompess_kenel">/dev/null
+# Create script to automatically decompress kernel (source: https://www.raspberrypi.org/forums/viewtopic.php?t=278791)
+echo "Creating script to automatically decompress kernel..."
+cat << \EOF | sudo tee "$mntBoot/auto_decompress_kernel">/dev/null
 #!/bin/bash -e
-# auto_decompess_kenel scipt
-BTPATH=/boot/fimwae
+# auto_decompress_kernel script
+BTPATH=/boot/firmware
 CKPATH=$BTPATH/vmlinuz
 DKPATH=$BTPATH/vmlinux
-# Check if compession needs to be done.
+# Check if compression needs to be done.
 if [ -e $BTPATH/check.md5 ]; then
-   if md5sum --status --ignoe-missing -c $BTPATH/check.md5; then
-      echo -e "\e[32mFiles have not changed, Decompession not needed\e[0m"
+   if md5sum --status --ignore-missing -c $BTPATH/check.md5; then
+      echo -e "\e[32mFiles have not changed, Decompression not needed\e[0m"
       exit 0
    else
-      echo -e "\e[31mHash failed, kenel will be compessed\e[0m"
+      echo -e "\e[31mHash failed, kernel will be compressed\e[0m"
    fi
 fi
-# Backup the old decompessed kenel
+# Backup the old decompressed kernel
 mv $DKPATH $DKPATH.bak
 if [ ! $? == 0 ]; then
    echo -e "\e[31mDECOMPRESSED KERNEL BACKUP FAILED!\e[0m"
    exit 1
 else
-   echo -e "\e[32mDecompessed kenel backup was successful\e[0m"
+   echo -e "\e[32mDecompressed kernel backup was successful\e[0m"
 fi
-# Decompess the new kenel
-echo "Decompessing kenel: "$CKPATH".............."
+# Decompress the new kernel
+echo "Decompressing kernel: "$CKPATH".............."
 zcat -qf $CKPATH > $DKPATH
 if [ ! $? == 0 ]; then
    echo -e "\e[31mKERNEL FAILED TO DECOMPRESS!\e[0m"
    exit 1
 else
-   echo -e "\e[32mKenel Decompessed Succesfully\e[0m"
+   echo -e "\e[32mKernel Decompressed Succesfully\e[0m"
 fi
-# Hash the new kenel fo checking
+# Hash the new kernel for checking
 md5sum $CKPATH $DKPATH > $BTPATH/check.md5
 if [ ! $? == 0 ]; then
    echo -e "\e[31mMD5 GENERATION FAILED!\e[0m"
 else
-   echo -e "\e[32mMD5 geneated Succesfully\e[0m"
+   echo -e "\e[32mMD5 generated Succesfully\e[0m"
 fi
 exit 0
 EOF
-sudo chmod +x "$mntBoot/auto_decompess_kenel"
+sudo chmod +x "$mntBoot/auto_decompress_kernel"
 
-# Ceate apt scipt to automatically decompess the kenel
-echo "Ceating apt scipt to automatically decompess kenel..."
-echo 'DPkg::Post-Invoke {"/bin/bash /boot/fimwae/auto_decompess_kenel"; };' | sudo tee "$mntWitable/etc/apt/apt.conf.d/999_decompess_pi_kenel" >/dev/null
-sudo chmod +x "$mntWitable/etc/apt/apt.conf.d/999_decompess_pi_kenel"
-
-sudo umount /mnt/boot
-sudo umount /mnt/witable
-sudo m -f /mnt/boot
-sudo m -f /mnt/witable
-histoy -c && histoy -w
+# Create apt script to automatically decompress the kernel
+echo "Creating apt script to automatically decompress kernel..."
+echo 'DPkg::Post-Invoke {"/bin/bash /boot/firmware/auto_decompress_kernel"; };' | sudo tee "$mntWritable/etc/apt/apt.conf.d/999_decompress_rpi_kernel" >/dev/null
+sudo chmod +x "$mntWritable/etc/apt/apt.conf.d/999_decompress_rpi_kernel"
 
 # Successful
-echo "Updating Ubuntu patition was successful!  Shut down you Pi, emove the SD cad then econnect the power."
+echo "Updating Ubuntu partition was successful!  Shut down your Pi, remove the SD card then reconnect the power."
