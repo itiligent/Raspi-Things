ccaaf00bcf12c10f605ec0b0927695396197a06a
itiligent <94789708+itiligent@users.noreply.github.com>
2022-07-17 14:55:41 +1000

    Update listen-for-shutdown.py

--- listen-for-shutdown.py
+++ listen-for-shutdown.py
@@ -9,3 +9,7 @@ GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.wait_for_edge(3, GPIO.FALLING)
 
 subprocess.call(['shutdown', '-h', 'now'], shell=False)
+
+# Install instructions:
+# sudo mv listen-for-shutdown.py /usr/local/bin/
+# sudo chmod +x /usr/local/bin/listen-for-shutdown.py
