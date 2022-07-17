f0bec373f4d513eb86c3b8233c53e84e7fb19aba
itiligent <94789708+itiligent@users.noreply.github.com>
2022-07-17 18:28:42 +1000

    Update setup-raspi-power-button.sh

--- setup-raspi-power-button.sh
+++ setup-raspi-power-button.sh
@@ -11,27 +11,30 @@
 wget https://raw.githubusercontent.com/itiligent/Raspi-Things/main/listen-for-shutdown.py -O listen-for-shutdown.py
 wget https://raw.githubusercontent.com/itiligent/Raspi-Things/main/listen-for-shutdown.sh -O listen-for-shutdown.sh
 
-mv listen-for-shutdown.py /usr/local/bin/
-mv listen-for-shutdown.sh /etc/init.d/
-
-chmod +x /usr/local/bin/listen-for-shutdown.py
-chmod +x /etc/init.d/listen-for-shutdown.sh
-
-update-rc.d listen-for-shutdown.sh defaults
-/etc/init.d/listen-for-shutdown.sh start
-
 # Enable serial gpio for Led power - Are we installing to Raspbian or Ubuntu?
 source /etc/os-release
 if [[ $ID = "debian" ]] || [[ $ID = "some other" ]]; then
 
 sed -i  '$ a enable_uart=1' /boot/config.txt
+mv listen-for-shutdown.py /usr/local/bin/
+mv listen-for-shutdown.sh /etc/init.d/
+chmod +x /usr/local/bin/listen-for-shutdown.py
+chmod +x /etc/init.d/listen-for-shutdown.sh
+update-rc.d listen-for-shutdown.sh defaults
+/etc/init.d/listen-for-shutdown.sh start
 
 else
 
 sed -i  '$ a enable_uart=1' /boot/firmware/config.txt
-sudo apt update
+sudo apt-get update
 sudo apt-get install python3-rpi.gpio
 sudo ln -s /usr/bin/python3 /usr/bin/python
+mv listen-for-shutdown.py /usr/local/bin/
+mv listen-for-shutdown.sh /etc/init.d/
+chmod +x /usr/local/bin/listen-for-shutdown.py
+chmod +x /etc/init.d/listen-for-shutdown.sh
+update-rc.d listen-for-shutdown.sh defaults
+/etc/init.d/listen-for-shutdown.sh start
 
 fi
 
